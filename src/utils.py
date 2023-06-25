0c7fa0cf99c68565468e7767bc0b3a674b5e1b8f
mykelismyname <michealabaho265@gmail.com>
2023-06-25 17:03:12 +0100

    helper functions for batching documents to process and computing token position given its first character offset

--- src/utils.py
+++ src/utils.py
@@ -2,16 +2,16 @@ import numpy as np
 
 #create spans of batches that would be used toselect chunks of documents that can be processed by Text analytics without which ratelimitting errors
 def batching_for_textanalyticsclinet(batch, size):
-  x,y = 0, batch
-  batch_list = []
-  for i in range(0,size,batch):
-    if i+batch >= size:
-      batch_list.append((x,x+(size-i)))
-      break
-    batch_list.append((x,y))
-    x = y
-    y = y + batch
-  return batch_list
+    x, y = 0, batch
+    batch_list = []
+    for i in range(0, size, batch):
+        if i + batch >= size:
+            batch_list.append((x, x + (size - i)))
+            break
+        batch_list.append((x, y))
+        x = y
+        y = y + batch
+    return batch_list
 
 #find the token id/position of an entity given its char offset from start of a sentence and the sentence
 def fecth_entitis_span_pos(entity, sentence):
@@ -34,7 +34,9 @@ def fecth_entitis_span_pos(entity, sentence):
         if end_span_found == True:
             if len(span) != 2:
                 print("Failed span", entity.text, span)
-            assert len(span) == 2
-            break
-
+            try:
+                assert len(span) == 2
+                break
+            except Exception as e:
+                return span
     return span
