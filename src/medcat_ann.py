423ee8bf1ae43435d429da2568e26826c720f7bc
mykelismyname <michealabaho265@gmail.com>
2023-07-05 19:33:00 +0100

    medcat model can now extract entities from text

--- src/medcat_ann.py
+++ src/medcat_ann.py
@@ -14,6 +14,9 @@ import re
 import time
 import pickle
 import logging
+from pyspark.sql import SparkSession
+import dask.dataframe as dd
+import utils
 
 pd.options.mode.chained_assignment = None
 logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
@@ -36,7 +39,7 @@ def update_concepts(model, concepts):
 def main(args):
     st_time = time.time()
     #load dataset
-    data_set = dd.read_csv(args.data, encoding="utf-8", engine="python", on_bad_lines="warn")
+    data_set = dd.read_csv(args.data_dir, encoding="utf-8", engine="python")
     data_ = data_set[["CLEANED_TEXT"]]
 
     annotation_size = args.annotation_size.split()
@@ -48,45 +51,47 @@ def main(args):
         _data_ = data_.compute()
         start, end = 0, len(_data_)
 
+    logging.info("Dataset successfully loaded")
+
     #load MEDCAT model
     medcat_model = CAT.load_model_pack(args.medcat_model)
 
     if args.update_concepts:
         concepts = args.concepts
-        model = update_concepts(medcat_model, concepts)
+        medcat_model = update_concepts(medcat_model, concepts)
 
     logging.info('---ANNOTATION BEGINS---')
     st_time = time.time()
     if not args.multiprocessing:
-        ann_file_.loc[:,'ENTITIES'] = ann_file_.loc[:,'CLEANED_TEXT'].apply(lambda x:ann_model.get_entities(x))
+        _data_.loc[:,'ENTITIES'] = ann_file_.loc[:,'CLEANED_TEXT'].apply(lambda x:medcat_model.get_entities(x))
         annotations = ann_file_['ENTITIES'].tolist()
     else:
         logging.info('---multi processing---')
-        annotations = ann_model.multiprocessing(data_iterator(ann_file_),
+        annotations = medcat_model.multiprocessing(data_iterator(_data_),
                                       batch_size_chars=args.batch_size_chars,
                                       nproc=8)
 
     dest = utils.createDir(args.dest)
     print('------------------------ANNOTATION ENDS------------------------')
-    et_time = time.time()
+    for i in annotations:
+        print(annotations[i])
+    # annotation_file = 'anns_mult_{}_{}.pkl'.format(start, end) if args.multiprocessing else 'anns_{}_{}.pkl'.format(start, end)
+    # with open(os.path.join(args.dest, annotation_file), 'wb') as a:
+    #     pickle.dump(annotations, a, protocol=pickle.HIGHEST_PROTOCOL)
+    #     a.close()
 
-    annotation_file = 'anns_mult_{}_{}.pkl'.format(start, end) if args.multiprocessing else 'anns_{}_{}.pkl'.format(start, end)
-    with open(os.path.join(args.dest, annotation_file), 'wb') as a:
-        pickle.dump(annotations, a, protocol=pickle.HIGHEST_PROTOCOL)
-        a.close()
-
-    logging.info("Total time taken {}s".format(time.time() - st))
+    logging.info("Total time taken {}s".format(time.time() - st_time))
 
 if __name__ == '__main__':
     par = ArgumentParser()
-    par.add_argument('--data', default='../NOTEEVENTS.csv.gz', help='data')
+    par.add_argument('--data_dir', default='../NOTEEVENTS.csv.gz', help='data')
     par.add_argument('--dest', default='../anns/', help='location to store annotation file')
     par.add_argument('--medcat_model', default='../models/medmen_wstatus_2021_oct.zip', type=str, help='model location')
     par.add_argument('--cleaning', action='store_true', help='clean the dataset or not')
-    par.add_argument('--annotation_size', type=str, help='number of documents to annotate')
+    par.add_argument('--annotation_size', default="-1", type=str, help='number of documents to annotate')
     par.add_argument('--update_concepts', action='store_true', help='update model concepts')
     par.add_argument('--concepts', default=['T047', 'T048', 'T200', 'T184'], type=list, help='concepts to annotate')
-    par.add_argument('--multiprocessing', default=False, type=bool, help='multiprocessing')
+    par.add_argument('--multiprocessing', action='store_true', help='multiprocessing')
     par.add_argument('--batch_size_chars', default=500000, type=int, help='Batch size (BS) in number of characters')
     args = par.parse_args()
     main(args)
