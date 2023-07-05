773e66d831254f0ff522969e724ac10dd25b13c2
mykelismyname <michealabaho265@gmail.com>
2023-07-05 19:33:43 +0100

    fix the bash running medcat

--- scripts/run_medcat_ann.sh
+++ scripts/run_medcat_ann.sh
@@ -1,5 +1,6 @@
 # bash scripts to use microsoft language text analytics to annotate data
-python annotate.py --data_dir ../NOTESEVENTS_CLEANED_5.csv \
+python ../src/medcat_ann.py --data_dir ../../mimic-iii/cleaned/NOTESEVENTS_CLEANED_TEXT_ONLY.csv \
 --annotation_size '0 20' \
 --dest '../anns' \
---model '../models/medmen_wstatus_2021_oct.zip' 
+--medcat_model '../medcat_models/medmen_wstatus_2021_oct.zip' \
+--multiprocessing
